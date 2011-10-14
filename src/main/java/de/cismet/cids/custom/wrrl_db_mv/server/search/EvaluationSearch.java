463603730dd24b5c11221ed0d177af26c864af73
Martin Scholl <martin.scholl@cismet.de>
2011-10-14 08:00:05 +0000

    [issue:2265] moved searches

---
+++ src/main/java/de/cismet/cids/custom/wrrl_db_mv/server/search/EvaluationSearch.java
@@ -0,0 +1,82 @@
+/***************************************************
+*
+* cismet GmbH, Saarbruecken, Germany
+*
+*              ... and it just works.
+*
+****************************************************/
+/*
+ *  Copyright (C) 2010 therter
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.cismet.cids.custom.wrrl_db_mv.server.search;
+
+import Sirius.server.middleware.interfaces.domainserver.MetaService;
+import Sirius.server.search.CidsServerSearch;
+
+import java.rmi.RemoteException;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import de.cismet.cids.custom.wrrl_db_mv.commons.WRRLUtil;
+
+/**
+ * Searchs for the wk_k the given geometry is contained in. The pgsql function getWk_k(integer, geometry) must exist in
+ * the database.
+ *
+ * @author   therter
+ * @version  $Revision$, $Date$
+ */
+public class EvaluationSearch extends CidsServerSearch {
+
+    //~ Instance fields --------------------------------------------------------
+
+    private String query;
+
+    //~ Constructors -----------------------------------------------------------
+
+    /**
+     * Creates a new WkkSearch object.
+     *
+     * @param  query  geometry DOCUMENT ME!
+     */
+    public EvaluationSearch(final String query) {
+        this.query = query;
+    }
+
+    //~ Methods ----------------------------------------------------------------
+
+    @Override
+    public Collection performServerSearch() {
+        final MetaService ms = (MetaService)getActiveLoaclServers().get(WRRLUtil.DOMAIN_NAME);
+
+        if (ms != null) {
+            try {
+                if (getLog().isDebugEnabled()) {
+                    getLog().debug("query: " + query); // NOI18N
+                }
+                final ArrayList<ArrayList> lists = ms.performCustomSearch(query);
+                return lists;
+            } catch (RemoteException ex) {
+                getLog().error(ex.getMessage(), ex);
+            }
+        } else {
+            getLog().error("active local server not found"); // NOI18N
+        }
+
+        return null;
+    }
+}
