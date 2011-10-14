4852e8c3992cdbeff2d1f8e6d79d0dd18b0847b1
Thorsten Herter <thorsten.herter@cismet.de>
2011-10-14 13:20:23 +0000

    [issue:0002162] add Custom Server Searches

---
+++ src/main/java/de/cismet/cids/custom/wrrl_db_mv/fgsk/server/search/WkFgLawaTypeSearch.java
@@ -0,0 +1,187 @@
+/***************************************************
+*
+* cismet GmbH, Saarbruecken, Germany
+*
+*              ... and it just works.
+*
+****************************************************/
+/*
+ *  Copyright (C) 2010 therter
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.cismet.cids.custom.wrrl_db_mv.fgsk.server.search;
+
+import Sirius.server.middleware.interfaces.domainserver.MetaService;
+import Sirius.server.search.CidsServerSearch;
+
+import java.rmi.RemoteException;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+/**
+ * Searchs for the LAWA types which are contained within a WK-FG.
+ *
+ * @author   therter
+ * @version  $Revision$, $Date$
+ */
+public class WkFgLawaTypeSearch extends CidsServerSearch {
+
+    //~ Static fields/initializers ---------------------------------------------
+
+    private static final String QUERY = "SELECT "
+                + "    nr.code, "
+                + "    nr.description, "
+                + "    wk_fg_total_length.length AS wk_fg_length, "
+                + "    length(st_intersection(wk_fg_total_geom, lg.geo_field)) AS intersection_length "
+                + "FROM "
+                + "    wk_fg AS fg, "
+                + "    lawa AS l, "
+                + "    station_linie AS linie, "
+                + "    geom AS lg, "
+                + "    la_lawa_nr AS nr, "
+                + "        ( "
+                + "            SELECT "
+                + "                sum(length(geo_field)) AS length, "
+                + "                geomunion(geo_field) AS wk_fg_total_geom, "
+                + "                extent(geo_field) AS wk_fg_total_geom_ext "
+                + "            FROM "
+                + "                wk_fg AS fg, "
+                + "                wk_fg_teile AS teile, "
+                + "                wk_teil AS teil, "
+                + "                station_linie AS linie, "
+                + "                geom AS g "
+                + "            WHERE "
+                + "                fg.id = %1$s "
+                + "                AND teil.id = teile.teil "
+                + "                AND teile.wk_fg_reference = fg.id "
+                + "                AND linie.id = teil.linie "
+                + "                AND g.id = linie.geom "
+                + "        ) wk_fg_total_length "
+                + "WHERE "
+                + "    fg.id = %1$s "
+                + "    AND linie.id = l.linie "
+                + "    AND linie.geom = lg.id "
+                + "    AND l.lawa_nr = nr.id "
+                + "    AND fast_intersects(lg.geo_field, wk_fg_total_geom_ext, wk_fg_total_geom)                               "
+                + "UNION                                                                                                     "
+                + "SELECT "
+                + "    -1, "
+                + "    'kein Typ',                                                                                    "
+                + "    coalesce( st_numGeometries( st_LineMerge( st_Difference( wk_fg_total_geom, st_union(lg.geo_field) ) ) ), 1), "
+                + "    st_length( st_LineMerge( st_Difference( wk_fg_total_geom, st_union(lg.geo_field) ) ) ) "
+                + "FROM "
+                + "    wk_fg AS fg, "
+                + "    lawa AS l, "
+                + "    station_linie AS linie, "
+                + "    geom AS lg, "
+                + "    la_lawa_nr AS nr,                                                            "
+                + "        ( "
+                + "            SELECT "
+                + "                sum(length(geo_field)) AS length, "
+                + "                geomunion(geo_field) AS wk_fg_total_geom, "
+                + "                extent(geo_field) AS wk_fg_total_geom_ext                                                             "
+                + "            FROM "
+                + "                wk_fg AS fg, "
+                + "                wk_fg_teile AS teile, "
+                + "                wk_teil AS teil, "
+                + "                station_linie AS linie, "
+                + "                geom g "
+                + "            WHERE "
+                + "                fg.id = %1$s "
+                + "                AND teil.id = teile.teil "
+                + "                AND teile.wk_fg_reference = fg.id "
+                + "                AND linie.id = teil.linie "
+                + "                AND g.id = linie.geom "
+                + "        ) wk_fg_total_length                                                        "
+                + "WHERE "
+                + "    fg.id = %1$s "
+                + "    AND linie.id = l.linie "
+                + "    AND linie.geom = lg.id "
+                + "    AND l.lawa_nr = nr.id "
+                + "    AND fast_intersects( lg.geo_field, wk_fg_total_geom_ext, wk_fg_total_geom ) "
+                + "GROUP BY "
+                + "    wk_fg_total_geom"
+                + ";"; // NOI18N
+
+    private static final String QUERY_WITHOUT_LAWA = "SELECT "
+                + "    -1, "
+                + "    'kein Typ', "
+                + "    sum( length( geo_field ) ) AS wk_fg_length, "
+                + "    sum( length( geo_field ) ) AS intersection_length "
+                + "FROM "
+                + "    wk_fg AS fg, "
+                + "    wk_fg_teile AS teile, "
+                + "    wk_teil AS teil, "
+                + "    station_linie AS linie, "
+                + "    geom AS g "
+                + "WHERE "
+                + "    fg.id = %1$s "
+                + "    AND teile.wk_fg_reference = fg.id "
+                + "    AND teil.id = teile.teil "
+                + "    AND linie.id = teil.linie "
+                + "    AND g.id = linie.geom;";             // NOI18N
+    private static final String WRRL_DOMAIN = "WRRL_DB_MV"; // NOI18N
+
+    //~ Instance fields --------------------------------------------------------
+
+    private String wkFgId;
+
+    //~ Constructors -----------------------------------------------------------
+
+    /**
+     * Creates a new LawaTypeNeighbourSearch object.
+     *
+     * @param  wkFgId  lawaTypeId DOCUMENT ME!
+     */
+    public WkFgLawaTypeSearch(final String wkFgId) {
+        this.wkFgId = wkFgId;
+    }
+
+    //~ Methods ----------------------------------------------------------------
+
+    @Override
+    public Collection performServerSearch() {
+        final MetaService ms = (MetaService)getActiveLoaclServers().get(WRRL_DOMAIN);
+
+        if (ms != null) {
+            try {
+                String query = String.format(QUERY, wkFgId);
+                if (getLog().isDebugEnabled()) {
+                    getLog().debug("query: " + query); // NOI18N
+                }
+                ArrayList<ArrayList> lists = ms.performCustomSearch(query);
+
+                if (!lists.isEmpty()) {
+                    return lists;
+                } else {
+                    query = String.format(QUERY_WITHOUT_LAWA, wkFgId);
+                    if (getLog().isDebugEnabled()) {
+                        getLog().debug("query: " + query); // NOI18N
+                    }
+                    lists = ms.performCustomSearch(query);
+
+                    return lists;
+                }
+            } catch (RemoteException ex) {
+                getLog().error(ex.getMessage(), ex);
+            }
+        } else {
+            getLog().error("active local server not found"); // NOI18N
+        }
+
+        return null;
+    }
+}
---
+++ src/main/java/de/cismet/cids/custom/wrrl_db_mv/fgsk/server/search/WkkSearch.java
@@ -0,0 +1,90 @@
+/***************************************************
+*
+* cismet GmbH, Saarbruecken, Germany
+*
+*              ... and it just works.
+*
+****************************************************/
+/*
+ *  Copyright (C) 2010 therter
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.cismet.cids.custom.wrrl_db_mv.fgsk.server.search;
+
+import Sirius.server.middleware.interfaces.domainserver.MetaService;
+import Sirius.server.search.CidsServerSearch;
+
+import java.rmi.RemoteException;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+/**
+ * Searchs for the wk_k the given geometry is contained in. The pgsql function getWk_k(integer, geometry) must exist in
+ * the database.
+ *
+ * @author   therter
+ * @version  $Revision$, $Date$
+ */
+public class WkkSearch extends CidsServerSearch {
+
+    //~ Static fields/initializers ---------------------------------------------
+
+    private static final String QUERY = "select getWk_k(%1$s, '%2$s');"; // NOI18N
+    private static final String WRRL_DOMAIN = "WRRL_DB_MV";              // NOI18N
+
+    //~ Instance fields --------------------------------------------------------
+
+    private String geometry;
+    private String route;
+
+    //~ Constructors -----------------------------------------------------------
+
+    /**
+     * Creates a new WkkSearch object.
+     *
+     * @param  geometry  DOCUMENT ME!
+     * @param  route     the route that contains the given geometry
+     */
+    public WkkSearch(final String geometry,
+            final String route) {
+        this.geometry = geometry;
+        this.route = route;
+    }
+
+    //~ Methods ----------------------------------------------------------------
+
+    @Override
+    public Collection performServerSearch() {
+        final MetaService ms = (MetaService)getActiveLoaclServers().get(WRRL_DOMAIN);
+
+        if (ms != null) {
+            try {
+                final String query = String.format(QUERY, route, geometry);
+                if (getLog().isDebugEnabled()) {
+                    getLog().debug("query: " + query); // NOI18N
+                }
+                final ArrayList<ArrayList> lists = ms.performCustomSearch(query);
+                return lists;
+            } catch (RemoteException ex) {
+                getLog().error(ex.getMessage(), ex);
+            }
+        } else {
+            getLog().error("active local server not found"); // NOI18N
+        }
+
+        return null;
+    }
+}
