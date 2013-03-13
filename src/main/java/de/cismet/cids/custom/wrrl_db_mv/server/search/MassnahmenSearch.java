e81840a47370160a6a40a9a8f762336442415f45
therter <thorsten.herter@cismet.de>
2013-03-13 09:46:19 +0100

    #7 los data model modifications

--- src/main/java/de/cismet/cids/custom/wrrl_db_mv/server/search/MassnahmenSearch.java
+++ src/main/java/de/cismet/cids/custom/wrrl_db_mv/server/search/MassnahmenSearch.java
@@ -40,7 +40,9 @@ public class MassnahmenSearch extends AbstractCidsServerSearch {
                 + " CASE WHEN a.aufmass_regel is null or a.aufmass_regel = '' THEN gewerk.aufmass_regel "
                 + "ELSE a.aufmass_regel END, CASE WHEN a.einheit is null or a.einheit = '' THEN gewerk.einheit "
                 + "ELSE a.einheit END, m.id, p.id, g.id from gup_unterhaltungsmassnahme m "
-                + "left outer join gup_planungsabschnitt p on (m.planungsabschnitt = p.id) "
+                + "join gup_los_gup_unterhaltungsmassnahme lgu on (m.id = lgu.gup_unterhaltungsmassnahme)"
+                + "left join gup_planungsabschnitt_gup_unterhaltungsmassnahme pgu on (m.id = pgu.gup_unterhaltungsmassnahme)"
+                + "left outer join gup_planungsabschnitt p on (pgu.gup_planungsabschnitt_reference = p.id) "
                 + "left outer join station_linie psl on (p.linie = psl.id) "
                 + "left outer join station pvon on (psl.von = pvon.id) "
                 + "left outer join station pbis on (psl.bis = pbis.id) "
@@ -52,7 +54,27 @@ public class MassnahmenSearch extends AbstractCidsServerSearch {
                 + "left join station von on (sl.von = von.id) "
                 + "left join station bis on (sl.bis = bis.id) "
                 + "left join route r on (von.route = r.id) "
-                + "WHERE m.los = %1$s";
+                + "WHERE lgu.gup_los_reference = %1$s";
+//    private static final String QUERY =
+//        "select pvon.wert, pbis.wert, g.name, p.name, a.name, b.ort, von.wert, bis.wert, "
+//                + "m.randstreifenbreite, m.boeschungsbreite, m.boeschungslaenge, "
+//                + "m.deichkronenbreite, m.sohlbreite, m.vorlandbreite, m.cbmprom, m.stueck, m.stunden, m.schnitttiefe, a.id, a.leistungstext, "
+//                + " CASE WHEN a.aufmass_regel is null or a.aufmass_regel = '' THEN gewerk.aufmass_regel "
+//                + "ELSE a.aufmass_regel END, CASE WHEN a.einheit is null or a.einheit = '' THEN gewerk.einheit "
+//                + "ELSE a.einheit END, m.id, p.id, g.id from gup_unterhaltungsmassnahme m "
+//                + "left outer join gup_planungsabschnitt p on (m.planungsabschnitt = p.id) "
+//                + "left outer join station_linie psl on (p.linie = psl.id) "
+//                + "left outer join station pvon on (psl.von = pvon.id) "
+//                + "left outer join station pbis on (psl.bis = pbis.id) "
+//                + "left outer join gup_gup g on (p.gup = g.id) "
+//                + "left outer join gup_massnahmenart a on (m.massnahme = a.id) "
+//                + "left join gup_massnahmenbezug b on (m.wo = b.id) "
+//                + "left join gup_gewerk gewerk on (a.gewerk = gewerk.id) "
+//                + "left join station_linie sl on (m.linie = sl.id) "
+//                + "left join station von on (sl.von = von.id) "
+//                + "left join station bis on (sl.bis = bis.id) "
+//                + "left join route r on (von.route = r.id) "
+//                + "WHERE m.los = %1$s";
 
     //~ Instance fields --------------------------------------------------------
 
--- src/main/java/de/cismet/cids/custom/wrrl_db_mv/server/trigger/GupTrigger.java
+++ src/main/java/de/cismet/cids/custom/wrrl_db_mv/server/trigger/GupTrigger.java
@@ -7,11 +7,16 @@
 ****************************************************/
 package de.cismet.cids.custom.wrrl_db_mv.server.trigger;
 
+import Sirius.server.localserver.DBServer;
 import Sirius.server.newuser.User;
+import Sirius.server.search.Query;
+import Sirius.server.search.QueryIdentifier;
 
 import org.openide.util.lookup.ServiceProvider;
 
 import java.sql.Statement;
+
+import java.util.List;
 
 import de.cismet.cids.dynamics.CidsBean;
 
@@ -34,6 +39,10 @@ public class GupTrigger extends AbstractDBAwareCidsTrigger {
             GupTrigger.class);
     private static final String GUP_LOS_CLASS_NAME = "de.cismet.cids.dynamics.Gup_los";
     private static final String GUP_LOS_TABLE_NAME = "gup_los";
+    private static final String LOS_QUERY =
+        "delete from gup_los_gup_unterhaltungsmassnahme where gup_unterhaltungsmassnahme = %1$s and gup_los_reference <> %2$s";
+    private static final String DELETE_UNASSIGNED_MASSN_QUERY =
+        "delete from gup_unterhaltungsmassnahme where id not in (select gup_unterhaltungsmassnahme from gup_planungsabschnitt_gup_unterhaltungsmassnahme)";
 
     //~ Methods ----------------------------------------------------------------
 
@@ -89,24 +98,75 @@ public class GupTrigger extends AbstractDBAwareCidsTrigger {
         return (cidsBean.getClass().getName().equals(GUP_LOS_CLASS_NAME));
     }
 
+    /**
+     * DOCUMENT ME!
+     *
+     * @param  cidsBean  DOCUMENT ME!
+     * @param  user      DOCUMENT ME!
+     */
+    private void unassignMassnFromOtherLos(final CidsBean cidsBean, final User user) {
+        // ensures that an object of the type gup_unterhaltungsmassnahme is only assigned to one gup_los
+        try {
+            final List<CidsBean> beans = cidsBean.getBeanCollectionProperty("massnahmen");
+            final Integer losId = (Integer)cidsBean.getProperty("id");
+            final DBServer server = getDbServer();
+            final Statement st = server.getActiveDBConnection().getConnection().createStatement();
+
+            if (beans != null) {
+                for (final CidsBean bean : beans) {
+                    final Integer massnId = (Integer)bean.getProperty("id");
+
+                    st.executeUpdate(String.format(LOS_QUERY, massnId.toString(), losId.toString()));
+                }
+            }
+        } catch (Exception e) {
+            log.error("Error while executing los trigger.", e);
+        }
+    }
+
+    /**
+     * DOCUMENT ME!
+     */
+    private void deleteUnassignedMassnObjects() {
+        // ensures that an object of the type gup_unterhaltungsmassnahme is only assigned to one gup_los
+        try {
+            final DBServer server = getDbServer();
+            final Statement st = server.getActiveDBConnection().getConnection().createStatement();
+
+            st.executeUpdate(DELETE_UNASSIGNED_MASSN_QUERY);
+        } catch (Exception e) {
+            log.error("Error while executing los trigger.", e);
+        }
+    }
+
     @Override
     public void afterCommittedInsert(final CidsBean cidsBean, final User user) {
+        if (isGupLos(cidsBean)) {
+            unassignMassnFromOtherLos(cidsBean, user);
+        }
     }
 
     @Override
     public void afterCommittedUpdate(final CidsBean cidsBean, final User user) {
+        if (isGupLos(cidsBean)) {
+            unassignMassnFromOtherLos(cidsBean, user);
+            deleteUnassignedMassnObjects();
+        }
     }
 
     @Override
     public void afterCommittedDelete(final CidsBean cidsBean, final User user) {
         if (isGupLos(cidsBean)) {
-            try {
-                final Object id = cidsBean.getProperty("id");
-                final Statement s = getDbServer().getActiveDBConnection().getConnection().createStatement();
-                s.execute("update gup_unterhaltungsmassnahme set los = null where los = " + id.toString());
-            } catch (Exception e) {
-                log.error("Error while executing los trigger.", e);
-            }
+            deleteUnassignedMassnObjects();
         }
+//        if (isGupLos(cidsBean)) {
+//            try {
+//                final Object id = cidsBean.getProperty("id");
+//                final Statement s = getDbServer().getActiveDBConnection().getConnection().createStatement();
+//                s.execute("update gup_unterhaltungsmassnahme set los = null where los = " + id.toString());
+//            } catch (Exception e) {
+//                log.error("Error while executing los trigger.", e);
+//            }
+//        }
     }
 }
