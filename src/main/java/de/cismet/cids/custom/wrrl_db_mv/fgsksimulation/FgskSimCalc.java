def5358fcf5813ce879697337b6546aec27f1306
therter <thorsten.herter@cismet.de>
2013-09-26 19:08:08 +0200

    #9 add custom search from sim_massnahmen objects

--- src/main/java/de/cismet/cids/custom/wrrl_db_mv/fgsksimulation/FgskSimCalc.java
+++ src/main/java/de/cismet/cids/custom/wrrl_db_mv/fgsksimulation/FgskSimCalc.java
@@ -29,7 +29,7 @@ public class FgskSimCalc {
     //~ Static fields/initializers ---------------------------------------------
 
     private static Logger LOG = Logger.getLogger(FgskSimCalc.class);
-    private static FgskSimCalc instance;
+    public static int THRESHHOLD = 90;
 
     //~ Constructors -----------------------------------------------------------
 
@@ -46,12 +46,109 @@ public class FgskSimCalc {
      *
      * @return  DOCUMENT ME!
      */
-    public synchronized FgskSimCalc getInstance() {
-        if (instance == null) {
-            instance = new FgskSimCalc();
-        }
+    public static FgskSimCalc getInstance() {
+        return LazyInitializer.INSTANCE;
+    }
 
-        return instance;
+    /**
+     * DOCUMENT ME!
+     *
+     * @param   wBean  DOCUMENT ME!
+     *
+     * @return  DOCUMENT ME!
+     */
+    public int calcFgskSum(final CidsBean wBean) {
+        int sum = 0;
+
+        sum += toInt((Integer)wBean.getProperty("anzahl_laengsbaenken_mvs"));
+        sum += toInt((Integer)wBean.getProperty("substratdiversitaet"));
+        sum += toInt((Integer)wBean.getProperty("kruemmungserosion"));
+        sum += toInt((Integer)wBean.getProperty("flaechennutzung"));
+        sum += toInt((Integer)wBean.getProperty("laufkruemmung"));
+        sum += toInt((Integer)wBean.getProperty("stroemungsdiversitaet"));
+        sum += toInt((Integer)wBean.getProperty("anzahl_querbaenke_mvs"));
+        sum += toInt((Integer)wBean.getProperty("anzahl_besonderer_uferstrukturen"));
+        sum += toInt((Integer)wBean.getProperty("gewaesserrandstreifen"));
+        sum += toInt((Integer)wBean.getProperty("anzahl_besonderer_laufstrukturen"));
+        sum += toInt((Integer)wBean.getProperty("sonstige_umfeldstrukturen"));
+        sum += toInt((Integer)wBean.getProperty("breitenvarianz"));
+        sum += toInt((Integer)wBean.getProperty("anzahl_besonderer_sohlstrukturen"));
+        sum += toInt((Integer)wBean.getProperty("sohltiefe_obere_profilbreite"));
+        sum += toInt((Integer)wBean.getProperty("tiefenvarianz"));
+        sum += toInt((Integer)wBean.getProperty("uferbewuchs"));
+        sum += toInt((Integer)wBean.getProperty("sohlverbau"));
+        sum += toInt((Integer)wBean.getProperty("uferverbau"));
+        sum += toInt((Integer)wBean.getProperty("breitenerosion"));
+        sum += toInt((Integer)wBean.getProperty("profiltyp"));
+
+        return sum;
+    }
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @param   wBean  DOCUMENT ME!
+     *
+     * @return  DOCUMENT ME!
+     */
+    public int calcBioSum(final CidsBean wBean) {
+        int sum = 0;
+
+        sum += toInt((Integer)wBean.getProperty("fische"));
+        sum += toInt((Integer)wBean.getProperty("makrozoobenthos"));
+        sum += toInt((Integer)wBean.getProperty("makrophyten"));
+
+        return sum;
+    }
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @param   kaBean  DOCUMENT ME!
+     * @param   rule    DOCUMENT ME!
+     *
+     * @return  DOCUMENT ME!
+     */
+    public boolean isRuleFulfilled(final CidsBean kaBean, final CidsBean rule) {
+        final String evalCode = (String)rule.getProperty("tester");
+        try {
+            if (evalCode != null) {
+                final String json = kaBean.toJSONString(false);
+                final ScriptEngineManager manager = new ScriptEngineManager();
+                final ScriptEngine engine = manager.getEngineByName("js");
+                String code = "var ka= " + json + ";";
+                code += "\n " + evalCode;
+
+                final Object result = engine.eval(code);
+
+                if (result instanceof Boolean) {
+                    return ((Boolean)result).booleanValue();
+                } else {
+                    LOG.warn("Test does not end with a boolean. Code: " + code);
+                    return false;
+                }
+            } else {
+                return true;
+            }
+        } catch (Exception ex) {
+            LOG.error("Error while check the following rule: " + evalCode, ex);
+            return false;
+        }
+    }
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @param   i  DOCUMENT ME!
+     *
+     * @return  DOCUMENT ME!
+     */
+    private int toInt(final Integer i) {
+        if (i == null) {
+            return 0;
+        } else {
+            return i.intValue();
+        }
     }
 
     /**
@@ -108,14 +205,20 @@ public class FgskSimCalc {
      * @return  DOCUMENT ME!
      */
     private String replaceVariables(final String formula, final CidsBean kaBean) {
-        final String breadth = (String)kaBean.getProperty("gewaesserbreite_id.name");
+        String breadth = (String)kaBean.getProperty("gewaesserbreite_id.name");
+        final Double bedBreadth = (Double)kaBean.getProperty("sohlenbreite");
         final Integer wbType = (Integer)kaBean.getProperty("gewaessertyp_id.value");
         final Double sohlsubstrKuenst = (Double)kaBean.getProperty("sohlensubstrat_kue");
+
+        if (breadth != null) {
+            breadth = "\"" + breadth + "\"";
+        }
 
         String newFormula = formula.replaceAll("LAENGE", String.valueOf(getKaLength(kaBean)));
         newFormula = newFormula.replaceAll("BREITE", String.valueOf(breadth));
         newFormula = newFormula.replaceAll("TYP", String.valueOf(wbType));
         newFormula = newFormula.replaceAll("SUBSTRAT", String.valueOf(sohlsubstrKuenst));
+        newFormula = newFormula.replaceAll("SOHLBREITE", String.valueOf(bedBreadth));
 
         return newFormula;
     }
@@ -129,7 +232,7 @@ public class FgskSimCalc {
      *
      * @throws  IllegalStateException  DOCUMENT ME!
      */
-    public static double getKaLength(final CidsBean kaBean) {
+    private double getKaLength(final CidsBean kaBean) {
         try {
             final Double toValue = (Double)kaBean.getProperty("linie.bis.wert");
             final Double fromValue = (Double)kaBean.getProperty("linie.von.wert");
@@ -139,6 +242,28 @@ public class FgskSimCalc {
             final String message = "illegal station settings in kartierabschnitt"; // NOI18N
             LOG.error(message, e);
             throw new IllegalStateException(message, e);
+        }
+    }
+
+    //~ Inner Classes ----------------------------------------------------------
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @version  $Revision$, $Date$
+     */
+    private static final class LazyInitializer {
+
+        //~ Static fields/initializers -----------------------------------------
+
+        private static final transient FgskSimCalc INSTANCE = new FgskSimCalc();
+
+        //~ Constructors -------------------------------------------------------
+
+        /**
+         * Creates a new LazyInitializer object.
+         */
+        private LazyInitializer() {
         }
     }
 }
--- src/main/java/de/cismet/cids/custom/wrrl_db_mv/server/search/MassnahmenvorschlagSearch.java
+++ src/main/java/de/cismet/cids/custom/wrrl_db_mv/server/search/MassnahmenvorschlagSearch.java
@@ -24,8 +24,12 @@ import java.rmi.RemoteException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 import de.cismet.cids.custom.wrrl_db_mv.commons.WRRLUtil;
+import de.cismet.cids.custom.wrrl_db_mv.fgsksimulation.FgskSimCalc;
 
 import de.cismet.cids.dynamics.CidsBean;
 
@@ -43,15 +47,58 @@ public class MassnahmenvorschlagSearch extends AbstractCidsServerSearch {
 
     /** LOGGER. */
     private static final transient Logger LOG = Logger.getLogger(MassnahmenvorschlagSearch.class);
-
-    private static final String QUERY =
-        "select distinct (select id from cs_class where name ilike 'sim_massnahmen_gruppe') , "
-                + "mg.id from sim_massnahmen_gruppe mg join sim_massnahmen_gruppe_sim_massnahmen smgsm on "
-                + "(mg.id = smgsm.sim_massnahmen_gruppe_reference)  join sim_massnahme m on (smgsm.massnahmen = m.id) "
-                + "join sim_massnahmen_wirkung w on m.id = w.massnahme join la_lawa_nr l "
-                + "on gewaessertyp = l.id where l.code = %1$s";
+    private static final String QUERY = "SELECT DISTINCT "
+                + "                (SELECT id "
+                + "                FROM    cs_class "
+                + "                WHERE   name ilike 'sim_massnahmenauswahl_regel' "
+                + "                ) "
+                + "                , "
+                + "                mr.id, "
+                + "                mg.name "
+                + "FROM            sim_massnahmenauswahl_regel mr "
+                + "                JOIN sim_massnahmenauswahl_regel_sim_massnahmen_gruppe smrsmg "
+                + "                ON              ( "
+                + "                                                mr.kandidaten = smrsmg.sim_massnahmenauswahl_regel_reference "
+                + "                                ) "
+                + "                JOIN sim_massnahmen_gruppe mg "
+                + "                ON              ( "
+                + "                                                smrsmg.massnahmengruppe = mg.id "
+                + "                                ) "
+                + "WHERE           ( "
+                + "                                (SELECT COUNT(DISTINCT m1.id) "
+                + "                                        FROM    sim_massnahmen_gruppe_sim_massnahmen smgsm1 "
+                + "                                                JOIN sim_massnahme m1 "
+                + "                                                ON      ( "
+                + "                                                                smgsm1.massnahmen = m1.id "
+                + "                                                        ) "
+                + "                                                JOIN sim_massnahmen_wirkung w "
+                + "                                                ON      m1.id = w.massnahme "
+                + "                                                JOIN la_lawa_nr l "
+                + "                                                ON      gewaessertyp            = l.id "
+                + "                                        WHERE   l.code                          = %1$s "
+                + "                                        AND     sim_massnahmen_gruppe_reference = mg.id "
+                + "                                ) "
+                + "                                = "
+                + "                                (SELECT COUNT(DISTINCT m2.id) "
+                + "                                FROM    sim_massnahmen_gruppe_sim_massnahmen smgsm2 "
+                + "                                        JOIN sim_massnahme m2 "
+                + "                                        ON      ( "
+                + "                                                        smgsm2.massnahmen = m2.id "
+                + "                                                ) "
+                + "                                WHERE   sim_massnahmen_gruppe_reference = mg.id "
+                + "                                ) "
+                + "                ) ";
 
     //~ Instance fields --------------------------------------------------------
+
+    // "select distinct (select id from cs_class where name ilike 'sim_massnahmenauswahl_regel'),"
+    // + " mr.id from sim_massnahmenauswahl_regel mr join sim_massnahmenauswahl_regel_sim_massnahmen_gruppe smrsmg"
+    // + " on (mr.kandidaten = smrsmg.sim_massnahmenauswahl_regel_reference) "
+    // + "join sim_massnahmen_gruppe mg on (smrsmg.massnahmengruppe = mg.id) "
+    // + " join sim_massnahmen_gruppe_sim_massnahmen smgsm on "
+    // + "(mg.id = smgsm.sim_massnahmen_gruppe_reference)  join sim_massnahme m on (smgsm.massnahmen = m.id) "
+    // + "join sim_massnahmen_wirkung w on m.id = w.massnahme join la_lawa_nr l "
+    // + "on gewaessertyp = l.id where l.code = %1$s";
 
     private MetaObject fgsk_mo;
     private User usr;
@@ -83,12 +130,74 @@ public class MassnahmenvorschlagSearch extends AbstractCidsServerSearch {
                 if (LOG.isDebugEnabled()) {
                     LOG.debug("query: " + query); // NOI18N
                 }
-                final MetaObject[] list = ms.getMetaObject(usr, query);
+                // Alle Regeln laden die auf Typ passen
+                final MetaObject[] rules = ms.getMetaObject(usr, query);
+                final ArrayList<CidsBean> resultCandidates = new ArrayList<CidsBean>(rules.length);
+                final ArrayList<MassnahmenEffizienz> wirkungen = new ArrayList<MassnahmenEffizienz>(rules.length);
 
-                final ArrayList<Node> result = new ArrayList<Node>(list.length);
+                final CidsBean kaBean = fgsk_mo.getBean();
 
-                for (final MetaObject tmpObject : list) {
-                    result.add(new MetaObjectNode(tmpObject.getBean()));
+                for (final MetaObject rule : rules) {
+                    if (FgskSimCalc.getInstance().isRuleFulfilled(kaBean, rule.getBean())) {
+                        final List<CidsBean> candidates = rule.getBean().getBeanCollectionProperty("kandidaten");
+                        final String hinweis = (String)rule.getBean().getProperty("hinweis");
+                        if (candidates != null) {
+                            for (final CidsBean candidate : candidates) {
+                                try {
+                                    candidate.setProperty("hinweis", hinweis);
+                                } catch (Exception e) {
+                                    LOG.error("Cannot set property hinweis", e);
+                                }
+                                if (isGroupAllowed(candidate, gewTyp)) {
+                                    resultCandidates.add(candidate);
+                                }
+                            }
+                        }
+                    }
+                }
+
+                for (final CidsBean massnahmegruppeBean : resultCandidates) {
+                    final List<CidsBean> einzelmassnahmen = new ArrayList<CidsBean>(
+                            massnahmegruppeBean.getBeanCollectionProperty("massnahmen"));
+                    double price = 0.0;
+                    int wirkung = 0;
+
+                    for (final CidsBean mn : einzelmassnahmen) {
+                        try {
+                            price += FgskSimCalc.getInstance().calcCosts(fgsk_mo.getBean(), mn);
+                            final List<CidsBean> massnahmeWirkungen = mn.getBeanCollectionProperty("wirkungen");
+
+                            for (final CidsBean wirkungBean : massnahmeWirkungen) {
+                                if (wirkungBean.getProperty("gewaessertyp.code").equals(gewTyp)) {
+                                    wirkung += FgskSimCalc.getInstance().calcFgskSum(wirkungBean);
+                                }
+                            }
+                        } catch (Exception e) {
+                            LOG.error("Cannot calculate the price", e);
+                        }
+                    }
+
+                    final MassnahmenEffizienz me = new MassnahmenEffizienz(wirkung, price, massnahmegruppeBean);
+                    if (!wirkungen.contains(me)) {
+                        wirkungen.add(me);
+                    }
+                }
+
+                Collections.sort(wirkungen, MassnahmenEffizienz.getComparator());
+
+                // rounding up
+                final int maxCount = Math.round(((float)wirkungen.size()
+                                    * (float)FgskSimCalc.THRESHHOLD
+                                    / 100f) + 0.5f);
+                int index = 0;
+                final ArrayList<Node> result = new ArrayList<Node>(maxCount);
+
+                for (final MassnahmenEffizienz tmpObject : wirkungen) {
+                    if (index == maxCount) {
+                        break;
+                    }
+                    result.add(new MetaObjectNode(tmpObject.getMgBean()));
+                    ++index;
                 }
 
                 return result;
@@ -100,5 +209,128 @@ public class MassnahmenvorschlagSearch extends AbstractCidsServerSearch {
         }
 
         return null;
+    }
+
+    /**
+     * Prüft, ob alle Mitglieder der Gruppe den gegebenen Gewässsertyp unterstützen.
+     *
+     * @param   group   DOCUMENT ME!
+     * @param   gewTyp  DOCUMENT ME!
+     *
+     * @return  DOCUMENT ME!
+     */
+    private boolean isGroupAllowed(final CidsBean group, final Object gewTyp) {
+        final List<CidsBean> massnList = group.getBeanCollectionProperty("massnahmen");
+
+        for (final CidsBean massn : massnList) {
+            boolean allowed = false;
+            final List<CidsBean> massnahmeWirkungen = massn.getBeanCollectionProperty("wirkungen");
+
+            for (final CidsBean wirkungBean : massnahmeWirkungen) {
+                if (wirkungBean.getProperty("gewaessertyp.code").equals(gewTyp)) {
+                    allowed = true;
+                    break;
+                }
+            }
+
+            if (!allowed) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    //~ Inner Classes ----------------------------------------------------------
+
+    /**
+     * This class is not comparable, because it cannot be ensured, that (x.compareTo(y)==0) == (x.equals(y)) and this
+     * can lead to problems, if a class is comparable. But the static method <code>getComparator()</code> delivers a
+     * Comparator class.
+     *
+     * @version  $Revision$, $Date$
+     */
+    private static class MassnahmenEffizienz {
+
+        //~ Instance fields ----------------------------------------------------
+
+        private CidsBean mgBean;
+        private double price;
+        private int wirkung;
+
+        //~ Constructors -------------------------------------------------------
+
+        /**
+         * Creates a new MassnahmenEffizienz object.
+         *
+         * @param  wirkung  DOCUMENT ME!
+         * @param  price    DOCUMENT ME!
+         * @param  mgBean   DOCUMENT ME!
+         */
+        public MassnahmenEffizienz(final int wirkung, final double price, final CidsBean mgBean) {
+            this.wirkung = wirkung;
+            this.price = price;
+            this.mgBean = mgBean;
+        }
+
+        //~ Methods ------------------------------------------------------------
+
+        @Override
+        public boolean equals(final Object obj) {
+            if (obj instanceof MassnahmenEffizienz) {
+                if (((MassnahmenEffizienz)obj).getMgBean().getMetaObject().getID()
+                            == getMgBean().getMetaObject().getID()) {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        @Override
+        public int hashCode() {
+            int hash = 7;
+            hash = (37 * hash)
+                        + ((this.mgBean != null) ? this.mgBean.hashCode() : 0);
+            return hash;
+        }
+
+        /**
+         * DOCUMENT ME!
+         *
+         * @return  the mgBean
+         */
+        public CidsBean getMgBean() {
+            return mgBean;
+        }
+
+        /**
+         * DOCUMENT ME!
+         *
+         * @param  mgBean  the mgBean to set
+         */
+        public void setMgBean(final CidsBean mgBean) {
+            this.mgBean = mgBean;
+        }
+
+        /**
+         * DOCUMENT ME!
+         *
+         * @return  DOCUMENT ME!
+         */
+        public static Comparator<MassnahmenEffizienz> getComparator() {
+            return new Comparator<MassnahmenEffizienz>() {
+
+                    @Override
+                    public int compare(final MassnahmenEffizienz o1, final MassnahmenEffizienz o2) {
+                        final double eff = o1.price
+                                    / (((double)o1.wirkung == 0.0) ? 1.0 : (double)o1.wirkung);
+                        final double otherEff = o2.price
+                                    / (((double)o2.wirkung == 0.0) ? 1.0 : (double)o2.wirkung);
+
+                        return (int)Math.signum(eff - otherEff);
+                    }
+                };
+        }
     }
 }
